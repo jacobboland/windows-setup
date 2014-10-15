a6b03c2dab041bde2178f6e9e79b43c0bae8a453
Jacob Boland <jacob.boland@gmail.com>
2014-10-14 17:20:04 -0700

    Add sourcetree to taskbar

--- boxstarter-dev.ps1
+++ boxstarter-dev.ps1
@@ -11,3 +11,4 @@ Install-ChocolateyPinnedTaskBarItem "C:\ProgramData\Microsoft\Windows\Start Menu
 Install-ChocolateyPinnedTaskBarItem "C:\Program Files\Microsoft Office 15\root\office15\lync.exe"
 Install-ChocolateyPinnedTaskBarItem "C:\ProgramData\chocolatey\lib\Atom.0.136.0\tools\Atom\atom.exe"
 Install-ChocolateyPinnedTaskBarItem "C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\IDE\devenv.exe"
+Install-ChocolateyPinnedTaskBarItem "C:\Program Files (x86)\Atlassian\SourceTree\SourceTree.exe"

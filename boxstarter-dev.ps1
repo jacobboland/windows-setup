6b93be7c6dee72eb2e12df3925b8c7db8d6fefc0
Jacob Boland <jacob.boland@gmail.com>
2014-12-14 15:12:02 -0800

    Add language support for html, javascript and less, with jshint and less linters

--- boxstarter-dev.ps1
+++ boxstarter-dev.ps1
@@ -6,7 +6,7 @@ cinst nodejs
 cinst nunit
 cinst paint.net
 cinst P4Merge
-cinst resharper
+cinst resharper -Version 8.2.3000.5176
 cinst SourceTree
 
 #Enable Web Services
@@ -20,13 +20,19 @@ cinst IIS-ISAPIExtensions -source WindowsFeatures
 Set-WindowsExplorerOptions -EnableShowHiddenFilesFoldersDrives -EnableShowProtectedOSFiles -EnableShowFileExtensions
 Set-TaskbarOptions -Size Small -Lock -Dock Bottom
 
-apm install vim-mode
+cup atom
+
+apm install language-html
+apm install language-javascript
+apm install language-less
 apm install linter
+apm install linter-jshint
+apm install linter-less
+apm install minimap
 apm install seti-ui
 apm install seti-syntax
-apm install minimap
+apm install vim-mode
 
-Install-ChocolateyPinnedTaskBarItem "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Microsoft Office"
 Install-ChocolateyPinnedTaskBarItem "C:\Program Files\Microsoft Office 15\root\office15\lync.exe"
 Install-ChocolateyPinnedTaskBarItem "C:\ProgramData\chocolatey\lib\Atom.0.136.0\tools\Atom\atom.exe"
 Install-ChocolateyPinnedTaskBarItem "C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\IDE\devenv.exe"
